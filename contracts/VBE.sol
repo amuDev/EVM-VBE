0e6ad92dd904350ebc99f0ab249a5dc35bd926f9
amuWin <12mcomco@gmail.com>
2022-04-16 10:54:58 -0500

    redesigned storage layout

--- contracts/VBE.sol
+++ contracts/VBE.sol
@@ -3,36 +3,39 @@ pragma solidity ^0.8.0;
 
 contract VBC {
     struct vbS {
+        // the first bytes equal to the amount of usable vars you have
+        // are reserved for pointers
+        // EX if you want to read the value of x
+        // you would first read the 4th byte of the storage slot
+        // that returns you the position of the first byte of that var
         uint256 a; // usable var = 0
         uint256 b; // usable var = 32
         uint256 c; // usable var = 64
         uint256 x; // usable var = 96
         uint256 y; // usable var = 128
         uint256 z; // usable var = 192
-        uint256 s; // required buffer var = 24
+        uint256 s; // required buffer var = 32
     }
 
     vbS public vbs;
 
+    /**
+      * @param action: Mathmatical action you want to perform on the value
+      * @param svar: What value you want to perform the action on
+      * @param num: The number you wish to use in your action
+      *
+      * Example:
+      *    action = 3 (division)
+      *    svar = 3 (x)
+      *    num = 2
+      *  The function will modify value x to equal div(x, 2)
+      */
     function VBE(
-        uint8 action, // Mathmatical action you want to perform on the value
-        uint8 svar,  // What value you want to perform the action on
-        uint256 num // The number you wish to use in your action
-        /* ex:
-            action = 3 (division)
-            svar = 3 (x)
-            num = 2
-
-         * The function will modify value x to equal div(x, 2)
-         */
+        uint8 action,
+        uint8 svar,
+        uint256 num
     ) public returns (uint256 newsvar) {
         assembly {
-            // Our first byte will be a length definer, this limits you to 7 32-words of usable storage since the last word is reserved for overflow
-            // effectivly stating the current number of bytes that are occupied.
-            // Since it cant be implied that are values are set
-            // If we are mutating a value that would have values inbetween
-            // then we must initalize the value with binary data of 10000000
-            // to specify that this byte is reserved for a value that is currently not being used.
             let ptr := sload(vbs.slot)
             let obc := and(ptr, 0xFF) // load occupied byte count into var
             switch obc
@@ -83,35 +86,33 @@ contract VBC {
     ) public view returns (bytes32 v) {
         assembly {
             let vptr := sload(vbs.slot)
-            let obc := byte(0, vptr) // load occupied byte count into var
-            let idx
+            // spos is agnostic to storage slots
+            // so we need to calculate what storage slot we are starting in
+            let spos := byte(svar, vptr) // get the starting position of svar
+            // what storage slot does our var start in
+            let sb := div(spos, 0x20)
+            let rem := mod(spos, 0x20)
+            switch sb
+            case 0 {}
+            default { vptr := sload(add(vbs.slot, sb)) spos := sub(spos, mul(sb, 0x20)) }
+
             // byte buffer
             let b := 0x0
-            // variables passed
-            let vc := 0x0
             // find the v we are looking for and vbd to fetch its val
-            for {idx := 1} lt(idx, obc) {idx := add(idx, 1)} {
+            for { let idx := 0 } lt(idx, 0x21) { idx := add(idx, 1) } {
                 // get the current byte
                 b := byte(idx, vptr)
-                if eq(svar, vc) {
-                    // append the last 7 bits of b to v
-                    switch v
-                    case 0 {
-                        v := b
-                    }
-                    default {
-                        v := or(shl(7, v), shr(1, b))
-                    }
-                    if gt(b, 0x7F) { break }
+                // append the last 7 bits of b to v
+                switch v
+                case 0 {
+                    v := shr(1, b)
                 }
+                default {
+                    v := or(shl(7, v), shr(1, b))
+                }
+                if gt(b, 0x7F) { break }
                 // if we are going into the next storage slot...
-                if eq(div(0x20, idx), 0) {
-                    vptr := sload(add(vbs.slot, div(idx, 0x20)))
-                }
-                // if this byte is the end of a var increase our counter
-                if gt(b, 0x7F) {
-                    vc := add(vc, 1)
-                }
+                if eq(rem, idx) { vptr := sload(add(vbs.slot, div(idx, 0x20))) }
             }
         }
     }
